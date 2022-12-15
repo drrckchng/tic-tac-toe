6e768c476b0d735d0ce3db0da45b9dd23ed55e76
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-12-15 13:23:27 +0900

    Add factory function for players

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Module for game board
 const gameBoard = (() => {
   const squares = () => document.querySelectorAll(".game-square");
   return { squares };
 })();
+
+// Player factory function
+const Player = (marker) => {
+  return { marker };
+}
+
+// Temp Player objects
+const player1 = Player("x");
+const player2 = Player("o");
