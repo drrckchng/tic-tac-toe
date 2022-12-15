589f7d90a053db6e23b66ae03608c3dde7c0d032
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-12-15 20:58:25 +0900

    Add form validity checker

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ const Player = (name, marker) => {
 // Temp Player objects
 const player1 = Player("jim", "x");
 const player2 = Player("bob", "o");
+
+// Grab submit button and add click event listener
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", userInput);
+
+function userInput(event) {
+  const name1 = document.getElementById("player-one-name").value;
+  const name2 = document.getElementById("player-two-name").value;
+  const xMarker = document.getElementById("x-marker").checked;
+  const oMarker = document.getElementById("o-marker").checked;
+  if(checkForm(name1, name2, xMarker, oMarker)) {
+    alert("run function here");
+  }
+  event.preventDefault();
+}
+
+function checkForm(name1, name2, xMarker, oMarker) {
+  if(name1 !== ""
+    && name2 !== ""
+    && ((xMarker === true && oMarker === false) || (xMarker === false && oMarker === true))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
