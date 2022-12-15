b06937072cc43476d17fbc99910c5141d42e1318
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-12-15 14:20:41 +0900

    Change arrow function return

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Module for game board
 const gameBoard = (() => {
-  const squares = () => document.querySelectorAll(".game-square");
+  let playerMarks = []; // Tracks player marks
+  const squares = () => {
+    const arr = document.querySelectorAll(".game-square");
+    return arr;
+  }
   return { squares };
 })();
+
+// Add event listener to squares
+gameBoard.squares().forEach(square => {
+  square.addEventListener("click", console.log("here"));
+});
+
+// for(let i = 0; i < gameBoard.squares().length; i++) {
+//   gameBoard.squares()[i].addEventListener("click", console.log("clicked"));
+// }
 
 // Module for display controller
 const display = (() => {
