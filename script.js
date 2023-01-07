430240bb07cd0bd7c79f9f6dca8d331d16882f27
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-01-07 20:48:06 +0900

    Move form handlers into form module

--- script.js
+++ script.js
@@ -88,41 +88,40 @@ const Player = (name, marker) => {
   return { name, marker };
 }
 
-// Grab submit button and add click event listener
-const submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", formInput);
-
-// Prevent default action of submit form
-// And runs form validity checker
-function formInput(event) {
-
-  const name1 = document.getElementById("player-one-name").value;
-  const name2 = document.getElementById("player-two-name").value;
-  const xMarker = document.getElementById("x-marker").checked;
-  const oMarker = document.getElementById("o-marker").checked;
-
-  if(checkForm(name1, name2, xMarker, oMarker)) {
-    // Check if player 1 mark is x or o
-    if(xMarker) {
-      gameBoard.player1 = Player(name1, "x");
-      gameBoard.player2 = Player(name2, "o");
+// Module for form
+const form = (() => {
+  // Prevent default action of submit form
+  // And runs form validity checker
+  const formInput = (event) => {
+    const name1 = document.getElementById("player-one-name").value;
+    const name2 = document.getElementById("player-two-name").value;
+    const xMarker = document.getElementById("x-marker").checked;
+    const oMarker = document.getElementById("o-marker").checked;
+    if(checkForm(name1, name2, xMarker, oMarker)) {
+      // Check if player 1 mark is x or o
+      if(xMarker) {
+        gameBoard.player1 = Player(name1, "x");
+        gameBoard.player2 = Player(name2, "o");
+      } else {
+        gameBoard.player1 = Player(name1, "o");
+        gameBoard.player2 = Player(name2, "x");
+      }
+    }
+    event.preventDefault(); // prevent default action
+  }
+  // Check if form is valid
+  const checkForm = (name1, name2, xMarker, oMarker) => {
+    if(name1 !== ""
+      && name2 !== ""
+      && ((xMarker === true && oMarker === false) || (xMarker === false && oMarker === true))
+    ) {
+      return true;
     } else {
-      gameBoard.player1 = Player(name1, "o");
-      gameBoard.player2 = Player(name2, "x");
+      return false;
     }
   }
-  event.preventDefault(); // prevent default action
-}
-
-// Check if form is valid
-function checkForm(name1, name2, xMarker, oMarker) {
-  if(name1 !== ""
-    && name2 !== ""
-    && ((xMarker === true && oMarker === false) || (xMarker === false && oMarker === true))
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
+  // Grab submit button and add click event listener
+  const submitButton = document.getElementById("submit");
+  submitButton.addEventListener("click", formInput);
+})();
 
